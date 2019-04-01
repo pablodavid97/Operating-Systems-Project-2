a6634243e6587c3900368cec69007c6470ae678a
Adrian Duque <adrianduque@localhost.localdomain>
2019-04-01 18:29:36 -0500

    Added wait to methods Deliver,Swap

--- src/TraderImpl.java
+++ src/TraderImpl.java
@@ -1,86 +1,89 @@
+import org.omg.CORBA.ORB;
+
 import java.util.*;
 
 public class TraderImpl implements Trader {
     private Grain specialty;
     private Map<Grain, Integer> bushels;
 
-    TraderImpl(Grain specialty){
+    TraderImpl(Grain specialty) {
         this.specialty = specialty;
         bushels = new EnumMap(Grain.class);
 
-        for(Grain g : Grain.values()){
+        for (Grain g : Grain.values()) {
             bushels.put(g, 0);
         }
     }
 
-    public synchronized void setBushelAmount(Grain g, int amt){
+    public void setBushelAmount(Grain g, int amt) {
         bushels.put(g, new Integer(amt));
     }
 
-    public void outOfStock(Grain g, int amt){
-        if(g.equals(specialty)){
+    public void outOfStock(Grain g, int amt) throws InterruptedException {
+        if (g.equals(specialty)) {
             deliver(Math.abs(amt));
         } else {
             try {
                 swap(g, Math.abs(amt));
-            }catch(Exception e){}
+            } catch (Exception e) {}
         }
     }
 
 
-    public void get(Order order){
+    public void get(Order order) throws InterruptedException {
         int diff = 0;
-        try {
-            for (Grain g : bushels.keySet()) {
-                
-                diff = bushels.get(g) - order.get(g);
-                if (diff < 0) {
-                    synchronized (order) {
-                        order.wait(1200);
-                    }
+        for (Grain g : bushels.keySet()) {
 
-                    synchronized (order) {
-                        outOfStock(g, diff);
-                        order.notifyAll();
-                    }
-                  //  outOfStock(g, diff);
+            diff = bushels.get(g) - order.get(g);
+            if (diff < 0) {
+                synchronized (order) {
+                    order.wait(1000);
+
+                }
+
+                synchronized (order) {
+                    outOfStock(g, diff);
+                    order.notify();
                 }
             }
-        }catch(Exception e){e.printStackTrace();}
+        }
+
     }
 
-    public void deliver(int amt){
+    public synchronized void deliver(int amt) throws InterruptedException {
         int qnty = bushels.get(specialty) + amt;
+        wait(5);
         setBushelAmount(specialty, qnty);
+        notify();
     }
 
-    public void swap(Grain what, int amt){
+    public void swap(Grain what, int amt) throws InterruptedException {
         int qnty1 = bushels.get(specialty) - amt;
         int qnty2 = bushels.get(what) + amt;
 
-
         setBushelAmount(specialty, qnty1);
         Order order = new Order();
-        synchronized (order){
+
+        synchronized (order) {
+            wait(5);
             order.set(what, amt);
+            notify();
         }
 
-
-        try {
-            P2.specialist(what).get(order);
-        } catch (InterruptedException e) {
-
+        synchronized (order) {
+            wait(5);
+            try {
+                P2.specialist(what).get(order);
+            } catch (InterruptedException e) {e.printStackTrace();}
+            setBushelAmount(what, qnty2);
+            notifyAll();
         }
-
-        setBushelAmount(what, qnty2);
     }
 
-    public Order getAmountOnHand(){
+    public Order getAmountOnHand() {
         Order order = new Order();
 
-        bushels.forEach((key, value) -> {
-            order.set(key, value);
-        });
+        bushels.forEach(order::set);
 
         return order;
     }
